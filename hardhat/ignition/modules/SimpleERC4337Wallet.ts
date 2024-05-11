2d322281c516f1435ca7fdb6d9a24e80606f4afb
Layr <layrfpf@gmail.com>
2024-05-11 19:00:19 -0300

    add comments to deploy scripts

--- hardhat/ignition/modules/SimpleERC4337Wallet.ts
+++ hardhat/ignition/modules/SimpleERC4337Wallet.ts
@@ -1,7 +1,8 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const SimpleERC4337WalletModule = buildModule("SimpleERC4337Wallet", (m) => {
-  const simpleERC4337Wallet = m.contract("SimpleERC4337Wallet", ["0x50A8C7f3D07BA8f05458F229D639c17C3D82D44A"]);
+  // update parameter with the local node's first signer's address
+  const simpleERC4337Wallet = m.contract("SimpleERC4337Wallet", ["0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"]);
   return { simpleERC4337Wallet };
 });
 
--- hardhat/ignition/modules/TestNFT.ts
+++ hardhat/ignition/modules/TestNFT.ts
@@ -1,7 +1,7 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const TestNFTModule = buildModule("TestNFTModule", (m) => {
-    // update value after deploying IntermediaryContract -> use its address
+    // update value after deploying IntermediaryContract -> we are going to use its address
     const intermediaryAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
     const testNFT = m.contract("TestNFT", [intermediaryAddress]);
     return { testNFT };
